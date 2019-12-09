20f5568dd4083f148dfe1aea214acf79b2f14d16
Cyclone35Jokesays <pesasebastian@gmail.com>
2019-12-09 15:48:21 -0800

    Player walk and click anim, world built with nav mesh, bombs need work, sword and attack animation not yet implemented.

---
+++ Assets/Scripts/BombExplosion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombExplosion : MonoBehaviour
+{
+    public static bool disabled = false;
+
+    void Update()
+    {
+      
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Bomb" )
+        {
+            Destroy(col.gameObject);
+            
+        }
+    }
+}
---
+++ Assets/Scripts/ClickMove.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickMove : MonoBehaviour
+{
+    private Animator anim;
+    private NavMeshAgent nAgent;
+    private bool amRunning = false;
+    private bool amAttacking = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        nAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if (Input.GetMouseButton(0))
+        {
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                nAgent.destination = hit.point;
+            }
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                nAgent.destination = hit.point;
+            }
+        }
+
+        if (nAgent.remainingDistance <= nAgent.stoppingDistance)
+        {
+            amRunning = false;
+            amAttacking = false;
+        }
+
+        else
+        {
+            amRunning = true;
+            amAttacking = true;
+        }
+        anim.SetBool("Walks", amRunning);
+        anim.SetBool("Walks", amAttacking);
+    }
+}
